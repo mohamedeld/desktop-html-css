697a871f671d5ee04932dece03cb4ff51e43b86f
mohamedeld <mohamed.azoz20010@gmail.com>
2024-07-17 22:09:44 +0300

    add login , signup with responsive

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const selectEL = document.querySelector(".selector");
+const options = document.querySelectorAll(".options");
+const selectImg = document.querySelector(".select-saudi")
+const list = document.querySelector(".list");
+selectEL.onclick = function() {
+    console.log("click")
+    list.classList.toggle("toggle-visible")
+}
+
+for (option of options) {
+    option.onclick = function() {
+        selectImg.innerHTML = this.innerHTML;
+        list.classList.toggle("toggle-visible")
+
+    }
+}
