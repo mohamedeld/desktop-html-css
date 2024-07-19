9fad1f6f2886a83f6c3206229e90c12e1e0d0ab3
mohamedeld <mohamed.azoz20010@gmail.com>
2024-07-19 20:03:57 +0300

    add 5 page in controller section

---
+++ scripts/edit_script.js
@@ -0,0 +1,7 @@
+const menuEl = document.querySelector(".menu");
+const asideEl = document.querySelector(".aside")
+if (menuEl !== null || menuEl !== undefined) {
+    menuEl.onclick = function() {
+        asideEl.classList.toggle("show-aside")
+    }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,5 @@ for (option of options) {
 
     }
 }
+
+// show menu and hide itdocument.
