c518197fac963743248a89a31d5dc2ec086cef87
mohamedeld <mohamed.azoz20010@gmail.com>
2024-07-29 21:06:23 +0300

    enhancement responsive and add upload image

--- scripts/edit_script.js
+++ scripts/edit_script.js
@@ -5,3 +5,28 @@ if (menuEl !== null || menuEl !== undefined) {
         asideEl.classList.toggle("show-aside")
     }
 }
+// show notifications
+
+const notificationEl = document.querySelector(".notification-container");
+const notificationItem = document.querySelector(".notification");
+notificationEl.onclick = function(){
+  this.classList.toggle("icon-not");
+  notificationItem.classList.toggle("show-notification")
+}
+//  upload image
+const addImageText = document.getElementById('add-image-text');
+const profileImage = document.getElementById('profile-image');
+const fileInput = document.getElementById('file-input');
+
+addImageText.addEventListener('click', () => {
+  fileInput.click();
+});
+
+fileInput.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    const imageUrl = URL.createObjectURL(file);
+    profileImage.src = imageUrl;
+    addImageText.style.display = 'none';
+  }
+});
--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,38 @@ for (option of options) {
 }
 
 // show menu and hide itdocument.
+
+// show notifications
+
+const notificationEl = document.querySelector(".notification-container");
+const notificationItem = document.querySelector(".notification");
+notificationEl.onclick = function(){
+  this.classList.toggle("icon-not");
+  notificationItem.classList.toggle("show-notification")
+}
+
+//  upload image
+const addImageText = document.getElementById('add-image-text');
+const profileImage = document.getElementById('profile-image');
+const fileInput = document.getElementById('file-input');
+
+addImageText.addEventListener('click', () => {
+  fileInput.click();
+});
+
+fileInput.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    const imageUrl = URL.createObjectURL(file);
+    profileImage.src = imageUrl;
+    addImageText.style.display = 'none';
+  }
+});
+
+const menuEl = document.querySelector(".menu");
+const asideEl = document.querySelector(".aside")
+if (menuEl !== null || menuEl !== undefined) {
+    menuEl.onclick = function() {
+        asideEl.classList.toggle("show-aside")
+    }
+}
