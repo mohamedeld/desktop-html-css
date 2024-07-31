021f79177ce36895158b308fb8a860e10a5dc731
mohamedeld <mohamed.azoz20010@gmail.com>
2024-07-31 22:45:19 +0300

    add some changes

--- scripts/edit_script.js
+++ scripts/edit_script.js
@@ -18,11 +18,11 @@ const addImageText = document.getElementById('add-image-text');
 const profileImage = document.getElementById('profile-image');
 const fileInput = document.getElementById('file-input');
 
-addImageText.addEventListener('click', () => {
+addImageText?.addEventListener('click', () => {
   fileInput.click();
 });
 
-fileInput.addEventListener('change', (event) => {
+fileInput?.addEventListener('change', (event) => {
   const file = event.target.files[0];
   if (file) {
     const imageUrl = URL.createObjectURL(file);
@@ -30,3 +30,30 @@ fileInput.addEventListener('change', (event) => {
     addImageText.style.display = 'none';
   }
 });
+const helpMessage = document.querySelector(".to-help");
+const redIcon = document?.querySelector("#redicon");
+
+if (redIcon) {
+  redIcon.addEventListener('mouseenter', function() {
+    if (helpMessage) {
+      helpMessage.classList.add("show-red-message");
+    }
+
+    // Add the full-screen background
+    const overlay = document.createElement('div');
+    overlay.classList.add('hover-overlay');
+    document.body.appendChild(overlay);
+  });
+
+  redIcon.addEventListener('mouseleave', function() {
+    if (helpMessage) {
+      helpMessage.classList.remove("show-red-message");
+    }
+
+    // Remove the full-screen background
+    const overlay = document.querySelector('.hover-overlay');
+    if (overlay) {
+      overlay.remove();
+    }
+  });
+}
